82eacc1342ba676a6cc6d5d69438561625f4f01f
nmohamed54321 <123846987+nmohamed54321@users.noreply.github.com>
2024-09-15 01:11:46 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "682b4282-75f3-4d35-912a-e17ff99c0973"} }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
